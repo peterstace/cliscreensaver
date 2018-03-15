bb2c3b3f44a902ab5cd120521587b6c525971bed
Peter Stace <peter@spaceship.com.au>
2018-03-15 21:28:16 +1100

    Add basic dithering

--- boxes.go
+++ boxes.go
@@ -28,21 +28,56 @@ func (s *screen) rowCol(i int) (int, int) {
 func (s *screen) print(w io.Writer) {
 	writeCursorPosition(w, 1, 1)
 	w.Write([]byte("\x1b[37;40m"))
-	for _, d := range s.data {
-		switch {
-		case d < 0.2:
+	for i := range s.data {
+		level, qerr := quantize(s.data[i])
+		switch level {
+		case 0:
 			w.Write([]byte(" "))
-		case d < 0.4:
+		case 1:
 			w.Write([]byte("░"))
-		case d < 0.6:
+		case 2:
 			w.Write([]byte("▒"))
-		case d < 0.8:
+		case 3:
 			w.Write([]byte("▓"))
-		default:
+		case 4:
 			w.Write([]byte("\x1b[30;47m \x1b[37;40m"))
+		default:
+			panic(level)
 		}
+
+		// TODO: Maybe do serpentine left/right.
+		r, c := s.rowCol(i)
+		qerr /= 16
+		if c != s.cols-1 {
+			s.data[i+1] += qerr * 7
+		}
+		if r != s.rows-1 {
+			if c != 0 {
+				s.data[i+s.cols-1] += qerr * 3
+			}
+			s.data[i+s.cols] += qerr * 5
+			if c != s.cols-1 {
+				s.data[i+s.cols+1] += qerr
+			}
+		}
+
 	}
 	w.Write([]byte("\x1b[m"))
+}
+
+func quantize(v float64) (int, float64) {
+	switch {
+	case v < 0.2:
+		return 0, v - 0.1
+	case v < 0.4:
+		return 1, v - 0.3
+	case v < 0.6:
+		return 2, v - 0.5
+	case v < 0.8:
+		return 3, v - 0.7
+	default:
+		return 4, v - 0.9
+	}
 }
 
 func termSize() (rows, cols int) {
